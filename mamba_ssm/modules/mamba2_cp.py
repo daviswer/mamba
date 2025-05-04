0f9266e1b9e2055b6f60126676fbed8fd3cf1fe2
Davis Wertheimer <dww78@cornell.edu>
2025-05-03 22:26:47 -0400

    Diag: big eps

--- mamba_ssm/modules/mamba2_cp.py
+++ mamba_ssm/modules/mamba2_cp.py
@@ -547,7 +547,7 @@ class Mamba2CP(Mamba2):
 
         # Apply denom
         y = y.view(*s[:-1], self.nheads, self.headdim)
-        denom = y[..., -1].abs() + 1e-5
+        denom = y[..., -1].abs() + .1
         y = y.div(denom.unsqueeze(-1))
         y[..., -1] = 0  # reserve
         y = y.view(*s)
